02ab835d70f3fbd4877fff37839f08e9464a57fe
vinodsagar260981 <vinodsagar0981@gmail.com>
2025-06-04 12:30:28 +0530

    flask app

---
+++ src/helper.py
@@ -0,0 +1,22 @@
+from langchain.document_loaders import DirectoryLoader
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain_community.document_loaders import PyPDFLoader
+from langchain_huggingface import HuggingFaceEmbeddings
+
+#Extract PDF from PDF file
+
+def load_pdf_file(data):
+    loader = DirectoryLoader(data,
+                            glob="*.pdf",
+                            loader_cls=PyPDFLoader)
+    documents=loader.load()
+    return documents
+
+def text_split(extracted_data):
+    text_splitter = RecursiveCharacterTextSplitter(chunk_size=500, chunk_overlap=20)
+    text_chunk = text_splitter.split_documents(extracted_data)
+    return text_chunk
+
+def downlaod_hugging_face_embedding():
+    embeddings = HuggingFaceEmbeddings(model_name='sentence-transformers/all-MiniLM-L6-v2')
+    return embeddings
